3fcb6af444ba7d393b19f40ed312434a63c7f8af
almeidarafael <almeidarafael@ua.pt>
2015-11-13 15:19:10 +0000

    mais probabilidades

--- casino.py
+++ casino.py
@@ -5,12 +5,14 @@ from student import StudentPlayer
 
 if __name__ == '__main__':
 
-    players = [StudentPlayer("Human",100)]
+    players = [StudentPlayer("Student",100)]
 
     for i in range(100):
         print players
-        g = Game(players, min_bet=1, max_bet=5) 
+        g = Game(players, min_bet=1, max_bet=5)
         #g = Game(players, debug=True)
         g.run()
 
     print "OVERALL: ", players
+
+
